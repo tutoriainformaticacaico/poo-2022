a6021bbc49f701dc66cedd25a3e7ff3eeb659326
GLAUCYRLEI PEREIRA DA SILVA <glaucyrlei.p@academico.ifrn.edu.br>
2022-12-20 07:39:33 -0300

    Gabarito prova 3° bim 2M

--- Gabarito Prova 3° bim - 2M/Estudante.java
+++ Gabarito Prova 3° bim - 2M/Estudante.java
@@ -15,10 +15,20 @@ public class Estudante {
         notas.add(new NotaDisciplina (nota, disciplina, ano));
     }
 
-    public boolean igualNota (Estudante outroEstudante, String disciplina) {
+    /* 
+     * A logica desse metodo eh simples, comparamos se uma nota eh igual a alguma nota do outro estudante.
+     * (por isso do for aninhado[dois for]), ou seja, comparamos uma posicao do atual estudante, com todas do 
+     * outro estudante, fazemos isso já que não temos como saber qual será a nota igual, ela pode estar
+     * tanto no começo, quanto no fim.
+     */
+    public boolean igualNota (Estudante outroEstudante) {
         for (int i = 0; i < notas.size(); i++) {
-            if (notas.get(i).getDisciplina().equals(disciplina)) {
-                return true;
+            for (int j = 0; j < outroEstudante.notas.size(); j++) {//pegando as notas do outro aluno
+                if (outroEstudante.getNotas().get(j).getNota() == notas.get(i).getNota()) {//verificando se existe uma nota igual
+                    if (outroEstudante.getNotas().get(j).getDisciplina().equals(notas.get(i).getDisciplina())) {//verificando se trata-se da mesma disciplina
+                        return true;//retorna true, oq indica que teve uma nota igual numa mesma materia
+                    }
+                }
             }
         }
         return false;
@@ -40,4 +50,24 @@ public class Estudante {
         }
     }
     
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNumEstudante() {
+        return numEstudante;
+    }
+    public void setNumEstudante(int numEstudante) {
+        this.numEstudante = numEstudante;
+    }
+
+    public ArrayList<NotaDisciplina> getNotas() {
+        return notas;
+    }
+    public void setNotas(ArrayList<NotaDisciplina> notas) {
+        this.notas = notas;
+    }
+
 }
