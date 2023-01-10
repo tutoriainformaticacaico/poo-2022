0ad68cfe29c19f5af60b0184e3b86044fbdf9e5b
GLAUCYRLEI PEREIRA DA SILVA <glaucyrlei.p@academico.ifrn.edu.br>
2023-01-10 08:19:17 -0300

    Main, sistema de login

--- Sistema de login/Main.java
+++ Sistema de login/Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Sistema s = new Sistema();
 		
 		System.out.println("---------Sistema de login----------");
+		System.out.println("Digite seu email:");
+		String email = sc.nextLine();
+		System.out.println("Digite sua senha");
+		String senha = sc.nextLine();
+		s.logar(email, senha);
 
 	}
 }
